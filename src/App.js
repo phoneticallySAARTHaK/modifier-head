9b6d10e6cccd01e86b02ab0b1e6daf35b8329e00
sarthak <sarthakbatham26@gmail.com>
2022-06-15 20:06:18 +0530

    try modal and button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import logo from './logo.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'reactstrap'
+import { Modal, ModalBody, ModalHeader } from 'reactstrap'
+import { useState } from 'react'
 
-function App() {}
+function App() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <Modal isOpen={open} toggle={() => setOpen((prevOpen) => !prevOpen)}>
+        <ModalHeader>Modal title</ModalHeader>
+        <ModalBody>Modal body text goes here.</ModalBody>
+      </Modal>
+      <Button color="danger" onClick={setOpen}>
+        Danger!
+      </Button>
+    </>
+  )
+}
 
 export default App
