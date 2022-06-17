6711fb017d167629b584b1d0567999fdd7b7dbe5
sarthak <sarthakbatham26@gmail.com>
2022-06-17 19:54:04 +0530

    update for heroku

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,11 @@
 import { Navbar } from 'reactstrap'
 
 function Header(props) {
-  return <Navbar></Navbar>
+  return (
+    <header>
+      <Navbar></Navbar>
+    </header>
+  )
 }
 
 export default Header
--- src/Main.js
+++ src/Main.js
@@ -3,10 +3,12 @@ import { Container, Row, Col } from 'reactstrap'
 // TODO
 function Main(props) {
   return (
-    <Container fluid="lg" className="bg-primary p-5">
-      <Row>
-        <Col>Some content</Col>
-      </Row>
+    <Container fluid="lg" className="bg-info p-5 text-center">
+      <h3 className="Display-3">What is this?</h3>
+      <h5 className="Display-5 text-muted">
+        Best demostrated with an example!
+      </h5>
+      <Row></Row>
     </Container>
   )
 }
